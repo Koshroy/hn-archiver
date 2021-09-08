2c338ec3e5d5593987c7fb565c6ec4f338db7321
Meff <meff@meff.me>
2021-09-08 01:08:52 -0700

    Add HN NNCP
    Add the tool to utilize fetch and generate to create a tarball with
    the HN archive.

---
+++ hn-nncp.py
@@ -0,0 +1,87 @@
+import argparse
+from data import Comment, Story
+from fetch import fetch
+from generate import generate
+import os
+from pathlib import Path
+import shutil
+import subprocess
+import sys
+import tarfile
+import tempfile
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Fetch all HackerNews posts')
+    parser.add_argument(
+        '--dump-file', metavar='dump_file', type=str,
+        help='Existing dump filename',
+        required=False,
+    )
+    parser.add_argument(
+        '--outgoing', metavar='outgoing', type=str,
+        help='Directory to place generated archive in for outgoing sends',
+        required=False,
+    )
+    
+
+    args = parser.parse_args()
+    dump_fname = args.dump_file
+    outgoing_dir = args.outgoing
+
+    dest_node = os.environ['NNCP_SENDER'] if not outgoing_dir else ''
+    nncp_dir = (os.environ.get('NNCP_DIR') + '/') or ''
+
+    if outgoing_dir:
+        num_stories = None
+    else:
+        try:
+            num_stories = int(sys.stdin.readline().strip())
+        except ValueError:
+            num_stories = None
+
+    if num_stories:
+        print(f'Packaging and sending {num_stories} stories')
+    else:
+        print(f'Packaging and sending all stories')
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        print(f'tmpdirname: {tmpdirname}')
+        tmp_path = Path(tmpdirname)
+        out_dir = tmp_path / 'hackernews'
+        out_dir.mkdir()
+        
+        dump_path = Path(dump_fname) if dump_fname else tmp_path / 'fetch.pickle'        
+        if not dump_fname:
+            print('Fetching new stories')
+            fetch(str(dump_path))
+
+        print('Generating output')
+        generate(str(dump_path), str(out_dir), num_stories)
+
+        tarpath = tmp_path / 'hackernews.tar.xz'
+        print('Creating tarfile')
+        with tarfile.open(tarpath, 'x:xz') as tar:
+            tar.add(str(out_dir), 'hackernews')
+
+        if outgoing_dir:
+            # Move the generated tarfile into the outgoing dir
+            print(f'Copying tarfile to outgoing dir {outgoing_dir}')
+            shutil.copy(tarpath, Path(outgoing_dir) / tarpath.name)
+        else:
+            # Instead, send it through nncp-file
+            print('Sending tarfile through NNCP')
+            proc = subprocess.run([
+                f"{nncp_dir}/nncp-file",
+                "-cfg",
+                "/tmp/nncp/bob.hjson",
+                str(tarpath),
+                f'{dest_node}:'
+            ])
+            if proc.returncode != 0:
+                print("Error: Could not run nncp-file")
+                sys.exit(proc.returncode)
+
+
+if __name__ == '__main__':
+    main()
